70b2f63b42a6f99851245beb839de423b07b7a93
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-11-27 13:07:53 +0100

    Add simple insertion sort example.

---
+++ List.scala
@@ -0,0 +1,45 @@
+package simple
+
+import leon.lang._
+import leon.annotation._
+
+object list {
+
+sealed abstract class List {
+
+  def size : BigInt = (this match {
+    case Nil => BigInt(0)
+    case Cons(_, xs) => 1 + xs.size
+  }) ensuring(_ >= 0)
+
+  def contents: Set[BigInt] = this match {
+    case Nil => Set.empty
+    case Cons(x,xs) => xs.contents ++ Set(x)
+  }
+
+  def isSorted: Boolean = this match {
+    case Nil => true
+    case Cons(x, Nil) => true
+    case Cons(x, Cons(y, ys)) => x <= y && Cons(y, ys).isSorted
+  }
+
+  /* Inserting element 'e' into a sorted list 'l' produces a sorted list with
+   * the expected content and size */
+  def sortedIns(e: BigInt): List = {
+    require(this.isSorted)
+    this match {
+      case Nil => Cons(e,Nil)
+      case Cons(x, xs) => if (x <= e) Cons(x, xs.sortedIns(e)) else Cons(e, this)
+    }
+  } ensuring(res => res.contents == this.contents ++ Set(e) 
+                    && res.isSorted
+                    && res.size == this.size + 1
+            )
+
+}
+
+case class Cons(head:BigInt, tail:List) extends List
+case object Nil extends List
+
+}
+
---
+++ Sort.scala
@@ -0,0 +1,28 @@
+package simple
+
+import leon.lang._
+import leon.annotation._
+
+object Sort {
+
+  import list._
+
+  /* Insertion sort yields a sorted list of same size and content as the input
+   * list */
+  def sort(l: List): List = (l match {
+    case Nil => Nil
+    case Cons(x,xs) => sort(xs).sortedIns(x)
+  }) ensuring(res => res.contents == l.contents 
+                     && res.isSorted
+                     && res.size == l.size
+             )
+
+  def main(args: Array[String]): Unit = {
+    val ls: List = Cons(5, Cons(2, Cons(4, Cons(5, Cons(1, Cons(8,Nil))))))
+
+    println(ls)
+    println(sort(ls))
+  }
+
+}
+
