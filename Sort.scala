5209d1a302609b9d56f0e6180f318edf62f413fc
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-02-26 15:49:07 +0100

    Back to normal

--- Sort.scala
+++ Sort.scala
@@ -21,8 +21,8 @@ object Sort {
   def main(args: Array[String]): Unit = {
     val ls: List = Cons(5, Cons(2, Cons(4, Cons(5, Cons(1, Cons(8,Nil))))))
 
-    // println(ls)
-    // println(sort(ls))
+    println(ls)
+    println(sort(ls))
   }
 
 }
